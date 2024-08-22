2784fc871335605b2912581a44d9fccc5c809317
Namburipardhasaradhi <namburipardhasaradhi@gmail.com>
2024-08-22 23:33:17 +0530

    Add Babel plugin for private properties in objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './components/register';
 import Login from './components/login';
 import Home from './components/home';
 import Contact from './components/contact';
 import Room from './components/meet/room';
 import RoomLogin from './components/meet/roomlogin';
-import PrivateRoute from './protectedroute';
+
+const PrivateRoute = ({ children }) => {
+    const userId = sessionStorage.getItem('userId');
+    return userId ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
@@ -16,7 +21,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<PrivateRoute><Contact /></PrivateRoute>} />
-        <Route path="/room/:roomID" element={<Room />} />
+        <Route path="/room/:roomID" element={<PrivateRoute><Room /></PrivateRoute>} />
         <Route path="/roomlogin" element={<PrivateRoute><RoomLogin /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
